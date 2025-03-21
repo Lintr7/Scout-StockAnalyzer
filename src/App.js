7c5d0ec8c0f7e78e37bb2ca5f757962aa791b5c7
Travis Lin <Lintr21@gmail.com>
2025-03-21 14:09:46 -0500

    Scout for localhost

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Pages
+import Homepage from './pages/Homepage';
+import StocksPage from './pages/StocksPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <main>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/stocks" element={<StocksPage />} />
+          </Routes>
+        </main>
+    </Router>
   );
 }
 
